ebe8cbff48afd933a6956f619ae910628e83b264
Codechef21 <34216156+Codechef21@users.noreply.github.com>
2019-11-07 00:53:44 +0600

    Add files via upload

---
+++ labexam2/labexam2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace labexam2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           // int[] arrayofNumbers;
+            int [] arrayofNumbers = { 2,3,4,5,6,7,8,9};
+            int total = Numbers.Multiply(arrayofNumbers);
+
+            Console.WriteLine("The result: {0} ", total);
+        }
+
+       
+        
+    }
+
+    public class Numbers
+    {
+        public static int Multiply(params int[] list)
+        {
+            int total = 1;
+            foreach (int num in list)
+            {
+                total *= num;
+            }
+
+            return total;
+        }
+    }
+}
